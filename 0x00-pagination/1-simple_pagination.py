58b9d3e3597809cf1ed1c9fa5f2f3a4ddcf46641
thedunncodes <tobioluwa348@gmail.com>
2024-04-03 13:18:36 +0000

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -38,8 +38,14 @@ class Server:
         """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
+        indexes = index_range(page, page_size)
+        try:
+            dataset = self.dataset()
+            new_page = []
+            index = indexes[0]
+            index_len = indexes[1] - indexes[0]
+            for i in range(index_len):
+                new_page.append(dataset[index + i])
+            return new_page
+        except IndexError:
             return []
-        return data[start:end]
