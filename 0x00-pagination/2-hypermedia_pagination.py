344358cee4d32838218f1fc94a2f1ef7228d897b
thedunncodes <tobioluwa348@gmail.com>
2024-04-03 13:09:25 +0000

    hyper pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -8,9 +8,8 @@ from typing import Tuple, List, Dict
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    '''
-    Returns index start and end page for the given range
-    '''
+    """Returns index start and end page for the given range
+    """
 
     return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
@@ -52,25 +51,25 @@ class Server:
                 return []
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Returns information about a page.
-        """
-        page_dict = {}
-        dataset = self.dataset()
-        data = self.get_page(page, page_size)
-        try:
-            page_dict['page_size'] = len(data)
-            page_dict['page'] = page
-            page_dict['data'] = data
-            if page < ((len(dataset) + 1) // page_size):
-                page_dict['next_page'] = page + 1
-            else:
-                page_dict['next_page'] = None
-            if not page <= 1:
-                page_dict['prev_page'] = page - 1
-            else:
-                page_dict['prev_page'] = None
-            page_dict['total_pages'] = math.ceil(len(dataset) / page_size)
+            """Returns information about a page.
+            """
+            page_dict = {}
+            dataset = self.dataset()
+            data = self.get_page(page, page_size)
+            try:
+                page_dict['page_size'] = len(data)
+                page_dict['page'] = page
+                page_dict['data'] = data
+                if page < ((len(dataset) + 1) // page_size):
+                    page_dict['next_page'] = page + 1
+                else:
+                    page_dict['next_page'] = None
+                if not page <= 1:
+                    page_dict['prev_page'] = page - 1
+                else:
+                    page_dict['prev_page'] = None
+                page_dict['total_pages'] = math.ceil(len(dataset) / page_size)
 
-            return page_dict
-        except IndexError:
-            return {}
+                return page_dict
+            except IndexError:
+                return {}
