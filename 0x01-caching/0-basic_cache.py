4de99f29168d511628b85b411015fe2a6af1524a
thedunncodes <tobioluwa348@gmail.com>
2024-04-06 12:25:30 +0000

    basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Basic Cache class
+"""
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        """ BasicCache init method
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Put method: adds a object into a dictionary
+        with the given key and item
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ get method: retreives an objece with the provided
+        key
+        """
+        return self.cache_data.get(key) if key or key in self.cache_data else\
+            None
